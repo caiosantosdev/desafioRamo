29e37cc2ecf59117d305a8cc6a3cc3f80fe555bf
Caio Santos <caiosdesouza23@gmail.com>
2023-10-16 23:11:46 -0300

    +fazendo uso dos metodos map e reduce // map - usado para imprimir somente os nomes do array. // reduce - usado para fazer parte da aritmetica de media do desafio.

--- desafio.js
+++ desafio.js
@@ -1,21 +1,50 @@
 var seuNome = "Guilherme";
 const objetos = [ 
-    {nome: "Garlos", idade: 4},
-    {nome: "Gabriel", idade: 3},
-    {nome: "Gabriela", idade: 3}
+    {nome: "Carlos", idade: 4},
+    {nome: "Gabriel", idade: 27},
+    {nome: "Gabriela", idade: 20}
 ];
 
 //atribui à "newArray" todos os objetos do array "objetos" cuja letra inicial é igual a letra inicial do nome do usuário.
 const newArray = objetos.filter(sameLeter => sameLeter.nome[0] == seuNome[0]);
-console.log(newArray);
-console.log(mediaDasIdades(newArray));
+//cria um novo array contendo só os nomes das pessoas com a mesma letra inicial obtida em "newArray".
+const nomes = newArray.map(item => item.nome);
+console.log(nomes);
+//faz a soma de todas as idades do array e atribui à variável media.
+let media = newArray.reduce((soma, item) => soma + item.idade , 0);
+media = (media / newArray.length).toFixed(2);
+console.log(`média: ${media}`);
+
+
+
+
+
+
+//COISAS USADAS PARA APRENDIZADO
 
 //faz a media das idades das pessoas presentes no novoArray.
-function mediaDasIdades(novoArray){
-    let somaIdades = 0;
-    for(let i = 0 ; i < novoArray.length ; i++){
-        somaIdades+=novoArray[i].idade;
-    }
-    let mediaIdades = somaIdades/(novoArray.length);
-    return mediaIdades.toFixed(2);
-}
+// function mediaDasIdades(novoArray){
+//     let somaIdades = 0;
+//     for(let i = 0 ; i < novoArray.length ; i++){
+//         somaIdades+=novoArray[i].idade;
+//     }
+//     let mediaIdades = somaIdades/(novoArray.length);
+//     return mediaIdades.toFixed(2);
+// }
+
+//poderia ter usado esse também, mas não sairia do jeito que o desafio pede.
+// newArray.forEach((item, index) => {console.log(item.nome)});
+
+
+//seuNome = seuNome.split(''); 
+
+// function filterSameFirstLetter(objetos){
+//     let arrayNomes = [];
+//     for(let i = 0 ; i< objetos.length ; i++){
+//         let auxNome = objetos[i].nome.split('');
+//         if(auxNome[0] == seuNome[0]){
+//             arrayNomes.push(objetos[i]);
+//         }
+//     }
+//     return arrayNomes;
+// }
