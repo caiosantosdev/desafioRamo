5c702e25cbbd842780e179da4689864e1552af0c
Caio Santos <caiosdesouza23@gmail.com>
2023-10-16 22:09:35 -0300

    commit final?

--- desafio.js
+++ desafio.js
@@ -1,35 +1,21 @@
 var seuNome = "Guilherme";
 const objetos = [ 
-    {nome: "Carlos", idade: 20},
-    {nome: "Gabriel", idade: 21},
-    {nome: "Gabriela", idade: 24}
+    {nome: "Garlos", idade: 4},
+    {nome: "Gabriel", idade: 3},
+    {nome: "Gabriela", idade: 3}
 ];
-// const novoArray = filterSameFirstLetter(objetos);
-// const mediaIdades = mediaDasIdades(novoArray);
+
+//atribui à "newArray" todos os objetos do array "objetos" cuja letra inicial é igual a letra inicial do nome do usuário.
 const newArray = objetos.filter(sameLeter => sameLeter.nome[0] == seuNome[0]);
 console.log(newArray);
 console.log(mediaDasIdades(newArray));
-// console.log(novoArray);
-// console.log(`A media de pessoas que tem a mesma letra inicial que voce: ${mediaIdades}`);
 
+//faz a media das idades das pessoas presentes no novoArray.
 function mediaDasIdades(novoArray){
     let somaIdades = 0;
     for(let i = 0 ; i < novoArray.length ; i++){
         somaIdades+=novoArray[i].idade;
     }
     let mediaIdades = somaIdades/(novoArray.length);
-    return mediaIdades;
+    return mediaIdades.toFixed(2);
 }
-//função que retorna o valor de objetos. // substitui a funcao do filter.
-// function filterSameFirstLetter(objetos){
-//     let arrayNomes = [];
-//     for(let i = 0 ; i < objetos.length ; i++){
-//         let auxNome = objetos[i].nome.split('');
-//         if(auxNome[0] == seuNome[0]){
-//             arrayNomes.push(objetos[i]);
-
-//         }
-//     }
-//     return arrayNomes;
-// }
-//funcao que faz a media das idades do array filtrado.
